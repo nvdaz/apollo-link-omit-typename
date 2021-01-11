a219dd5dce027a91a5400a17427f5ebafd870a40
Kyle <kawigdd@gmail.com>
2021-01-11 14:59:59 -0500

    remove constructor params

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,28 +1,44 @@
-import { ApolloLink, execute, Operation } from '@apollo/client/link/core';
+import { Observable } from '@apollo/client/core';
+import {
+  ApolloLink,
+  execute,
+  NextLink,
+  Operation,
+} from '@apollo/client/link/core';
+import { createOperation, transformOperation } from '@apollo/client/link/utils';
+import { setContext } from '@apollo/client/link/context';
 import gql from 'graphql-tag';
 
 import OmitTypenameLink from '..';
+import deepOmit from '../deepOmit';
 
 class TestLink extends ApolloLink {
   public operation: Operation;
 
-  request(operation: Operation) {
+  request(operation: Operation, forward: NextLink) {
     this.operation = operation;
+
+    if (forward) {
+      return forward(operation);
+    }
   }
 }
 
 describe('OmitTypenameLink', () => {
+  let prevLink: TestLink;
   let omitTypenameLink: OmitTypenameLink;
-  let testLink: TestLink;
+  let postLink: TestLink;
+
   let link: ApolloLink;
 
   beforeEach(() => {
+    prevLink = new TestLink();
     omitTypenameLink = new OmitTypenameLink();
-    testLink = new TestLink();
-    link = ApolloLink.from([omitTypenameLink, testLink]);
+    postLink = new TestLink();
+    link = ApolloLink.from([prevLink, omitTypenameLink, postLink]);
   });
 
-  it('omits typename from variables', () => {
+  it('deep omits typename from variables', () => {
     return new Promise<void>((resolve) => {
       execute(link, {
         query: gql`
@@ -33,11 +49,82 @@ describe('OmitTypenameLink', () => {
         variables: { v: { __typename: 'T', a: 'a' } },
       }).subscribe({
         complete() {
-          expect('__typename' in testLink.operation.variables.v).toBe(false);
+          expect(
+            deepOmit(prevLink.operation.variables, '__typename')
+          ).toMatchObject(postLink.operation.variables);
 
           resolve();
         },
       });
     });
+  });
+
+  it('persists context', () => {
+    return new Promise<void>((resolve) => {
+      execute(
+        ApolloLink.from([
+          setContext(() => ({
+            z: 'z',
+          })),
+          link,
+        ]),
+        {
+          query: gql`
+            query Q {
+              q
+            }
+          `,
+        }
+      ).subscribe({
+        complete() {
+          expect(typeof prevLink.operation.getContext).toBe('function');
+          expect(typeof postLink.operation.getContext).toBe('function');
+
+          expect(prevLink.operation.getContext()).toMatchObject(
+            postLink.operation.getContext()
+          );
+
+          resolve();
+        },
+      });
+    });
+  });
+
+  it('returns forwarded value', () => {
+    const value = Observable.of();
+
+    const operation = createOperation(
+      undefined,
+      transformOperation({
+        query: gql`
+          query Q {
+            q
+          }
+        `,
+      })
+    );
+
+    const result = omitTypenameLink.request(operation, () => value);
+
+    expect(result).toStrictEqual(value);
+  });
+
+  it('forwards the operation', () => {
+    const forward = jest.fn();
+
+    const operation = createOperation(
+      undefined,
+      transformOperation({
+        query: gql`
+          query Q {
+            q
+          }
+        `,
+      })
+    );
+
+    omitTypenameLink.request(operation, forward);
+
+    expect(forward).toBeCalledWith(operation);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-import { ApolloLink, NextLink, Operation } from '@apollo/client/link/core';
+import { Observable } from '@apollo/client/core';
+import {
+  ApolloLink,
+  FetchResult,
+  NextLink,
+  Operation,
+} from '@apollo/client/link/core';
 import deepOmit from './deepOmit';
 
 export default class OmitTypenameLink extends ApolloLink {
-  request(operation: Operation, forward: NextLink) {
+  constructor() {
+    super();
+  }
+
+  request(operation: Operation, forward: NextLink): Observable<FetchResult> {
     operation.variables = deepOmit(operation.variables, '__typename');
 
     return forward(operation);
